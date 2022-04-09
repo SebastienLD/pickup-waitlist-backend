d5d41397dff828f75ae6f46ee0be31cd8644c256
sirajabbasi200 <stabbasi@uci.edu>
2022-04-09 14:17:49 -0700

    Second commit

--- api/routes.py
+++ api/routes.py
@@ -3,6 +3,8 @@ from flask import request
 
 from app import app, db
 from models.check_in import CheckIn
+from models.team import Team
+from models.player import Player
 
 @app.route('/check_in', methods=['POST'])
 def check_in():
@@ -15,6 +17,26 @@ def check_in():
         )
     )
     return {}
+
+@app.route('/call_next', methods=['POST'])
+def check_in():
+    data = request.get_json()
+    new_team = Team(created=datetime.now())
+    new_player = Player(name=data["name"], team_id=new_team.team_id)
+    
+    db.session.add_all([new_team, new_player])
+    db.session.commit()
+    return {}
+
+@app.route('/join_team', methods=['POST'])
+def join_team():
+    data = request.get_json()
+    new_player = Player(name=data["name"], team_id=data["teamId"])
+    
+    db.session.add(new_player)
+    db.session.commit()
+    return {}
+
 
 
 @app.route('/test')
--- models/player/model.py
+++ models/player/model.py
@@ -4,5 +4,6 @@ from ..base import Base
 class Player(Base):
     __tablename__ = 'player'
     player_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer())
+    ##user_id = db.Column(db.Integer())
     team_id = db.Column(db.Integer())
+    name = db.Column(db.String())
